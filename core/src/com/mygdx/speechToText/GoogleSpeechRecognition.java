ba3388f83707779f633f4d6ab8b28476ae674750
cenzowm <cenzowm@DESKTOP-T05AH3Q>
2019-01-03 17:29:55 +0100

    SpeechToText should to work (logic at least)
    + logic of STT should to work only in logic
    +add 4 jar in "jars" folder (please add it on your build path)
    
    TODO:
    -where I have to save the result of STT?
    -simple button to add in the main screen to test the SST

---
+++ core/src/com/mygdx/speechToText/GoogleSpeechRecognition.java
@@ -0,0 +1,65 @@
+package com.mygdx.speechToText;
+
+import com.darkprograms.speech.microphone.Microphone;
+import com.darkprograms.speech.recognizer.GSpeechDuplex;
+import com.darkprograms.speech.recognizer.GSpeechResponseListener;
+import com.darkprograms.speech.recognizer.GoogleResponse;
+
+import net.sourceforge.javaflacencoder.FLACFileWriter;
+
+public class GoogleSpeechRecognition implements GSpeechResponseListener {
+
+	public static void main(String[] args) {
+
+		// microfono
+		final Microphone mic = new Microphone(FLACFileWriter.FLAC);
+		// creiamo il duplex con la relativa chiave dei servizi Google Cloud
+		GSpeechDuplex duplex = new GSpeechDuplex("AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw");
+
+		// settiamo la lingua
+		duplex.setLanguage("it"); // funziona anche in inglese
+
+	}
+
+	// funzione che fa iniziare il riconoscimento vocale
+	public void startingSpeechRecognition(GSpeechDuplex duplex, Microphone mic) {
+		try {
+			duplex.recognize(mic.getTargetDataLine(), mic.getAudioFormat());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	// funziona che stoppa il riconoscimento vocale
+	public void stopSpeechRecognition(GSpeechDuplex duplex, Microphone mic) {
+		mic.close();
+		duplex.stopSpeechRecognition();
+	}
+
+	// con questo metodo ricaviamo il testo della cattura audio
+	public static void getResponse(GSpeechDuplex duplex) {
+		duplex.addResponseListener(new GSpeechResponseListener() {
+
+			public void onResponse(GoogleResponse gr) {
+				String output = "";
+				output = gr.getResponse();
+				if (output.contains("(")) {
+					output = output.substring(0, output.indexOf('('));
+				}
+				if (!gr.getOtherPossibleResponses().isEmpty()) {
+					output = output + " (" + (String) gr.getOtherPossibleResponses().get(0) + ")";
+				}
+
+				// stampo in console quello che ha capito il riconoscitore
+				System.out.println(output);
+			}
+
+		});
+
+	}
+
+	// gia' implementata
+	@Override
+	public void onResponse(GoogleResponse gr) {
+	}
+}
