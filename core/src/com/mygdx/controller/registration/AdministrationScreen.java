7cdbb122ebf8d52ac61f7401324643624d9ec438
cenzowm <cenzowm@192.168.1.16>
2019-01-25 16:29:02 +0100

    Edit adm screen adding user managing

--- core/src/com/mygdx/controller/registration/AdministrationScreen.java
+++ core/src/com/mygdx/controller/registration/AdministrationScreen.java
@@ -2,7 +2,11 @@ package com.mygdx.controller.registration;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -13,9 +17,11 @@ import com.mygdx.simulator.factory_methos_screens.RegistrationCredentialsScreenC
 
 import utilis.Utils;
 
-public class AdministrationScreen extends AbstractScreen{
+public class AdministrationScreen extends AbstractScreen {
 
 	private Table rightTable;
+
+	private Table centerTable;
 
 	private TextButton registrationButton;
 	private TextButton showHouseInteractionButton;
@@ -34,11 +40,14 @@ public class AdministrationScreen extends AbstractScreen{
 		rightTable = new Table();
 		rightTable.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		rightTable.center();
-		rightTable.debug();
+		
+
+		centerTable = new Table();
+		centerTable.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		centerTable.center();
+		
 	}
 
-	
-	
 	@Override
 	public void show() {
 
@@ -53,25 +62,138 @@ public class AdministrationScreen extends AbstractScreen{
 		// In ascolto di eventi
 		this.registrationButton.addListener(new ClickListener() {
 			@Override
-				public void clicked(InputEvent event, float x, float y) {
-					registration = true;
-				}
+			public void clicked(InputEvent event, float x, float y) {
+				registration = true;
+			}
 		});
 
 		this.showHouseInteractionButton.addListener(new ClickListener() {
 			@Override
-				public void clicked(InputEvent event, float x, float y) {
-					showHouseInteraction = true;
-				}
+			public void clicked(InputEvent event, float x, float y) {
+				showHouseInteraction = true;
+			}
 		});
 
 		this.backButton.addListener(new ClickListener() {
 			@Override
-				public void clicked(InputEvent event, float x, float y) {
-					back = true;
-				}
+			public void clicked(InputEvent event, float x, float y) {
+				back = true;
+			}
 		});
 
+		Label nameLabel = new Label("User:", skin);
+		Image img_user_unclicked = new Image(new Texture("resources/images/a3.jpg"));
+		Image img_user_clicked = new Image(new Texture("resources/images/a3.jpg"));
+		ImageButton user1 = new ImageButton(img_user_unclicked.getDrawable(), img_user_clicked.getDrawable());
+		user1.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y) {
+				// accedo al panel dell'utente
+			}
+
+		});
+
+		Label nameLabel2 = new Label("User:", skin);
+		Image img_user_unclicked2 = new Image(new Texture("resources/images/a3.jpg"));
+		Image img_user_clicked2 = new Image(new Texture("resources/images/a3.jpg"));
+		ImageButton user2 = new ImageButton(img_user_unclicked2.getDrawable(), img_user_clicked2.getDrawable());
+		user2.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y) {
+				// accedo al panel dell'utente
+			}
+
+		});
+		
+		Label nameLabel3 = new Label("User:", skin);
+		Image img_user_unclicked3= new Image(new Texture("resources/images/a3.jpg"));
+		Image img_user_clicked3 = new Image(new Texture("resources/images/a3.jpg"));
+		ImageButton user3= new ImageButton(img_user_unclicked3.getDrawable(), img_user_clicked3.getDrawable());
+		user3.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y) {
+				// accedo al panel dell'utente
+			}
+
+		});
+		
+		
+		Label nameLabel4 = new Label("User:", skin);
+		Image img_user_unclicked4 = new Image(new Texture("resources/images/a3.jpg"));
+		Image img_user_clicked4 = new Image(new Texture("resources/images/a3.jpg"));
+		ImageButton user4 = new ImageButton(img_user_unclicked4.getDrawable(), img_user_clicked4.getDrawable());
+		user4.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y) {
+				// accedo al panel dell'utente
+			}
+
+		});
+		
+		Label nameLabel5 = new Label("User:", skin);
+		Image img_user_unclicked5 = new Image(new Texture("resources/images/a3.jpg"));
+		Image img_user_clicked5 = new Image(new Texture("resources/images/a3.jpg"));
+		ImageButton user5 = new ImageButton(img_user_unclicked5.getDrawable(), img_user_clicked5.getDrawable());
+		user5.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y) {
+				// accedo al panel dell'utente
+			}
+
+		});
+		
+		Label nameLabel6 = new Label("User:", skin);
+		Image img_user_unclicked6 = new Image(new Texture("resources/images/a3.jpg"));
+		Image img_user_clicked6 = new Image(new Texture("resources/images/a3.jpg"));
+		ImageButton user6 = new ImageButton(img_user_unclicked6.getDrawable(), img_user_clicked6.getDrawable());
+		user6.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y) {
+				// accedo al panel dell'utente
+			}
+
+		});
+
+		// label per distanziare lol
+		Label dist = new Label("", skin);
+
+		centerTable.add(nameLabel);
+		centerTable.add(dist).width(62);
+		centerTable.add(nameLabel2);
+
+		// 1-1
+		centerTable.row();
+		centerTable.add(user1).size(120, 120);
+
+		centerTable.add(dist).width(62);
+		// 1-2
+		centerTable.add(user2).size(120, 120).width(120);
+		centerTable.row();
+
+		centerTable.add(nameLabel3);
+		centerTable.add(dist).width(62);
+		centerTable.add(nameLabel4);
+		// 2-1
+		centerTable.row();
+		centerTable.add(user3).size(120, 120);
+
+		centerTable.add(dist).width(62);
+		// 2-2
+		centerTable.add(user4).size(120, 120).width(120);
+		centerTable.row();
+
+		centerTable.add(nameLabel5);
+		centerTable.add(dist).width(62);
+		centerTable.add(nameLabel6);
+		
+		// 3-1
+		centerTable.row();
+		centerTable.add(user5).size(120, 120);
+		
+		centerTable.add(dist).width(62);
+		// 3-2
+		centerTable.add(user6).size(120, 120).width(120);
+		centerTable.row();
+		
+		
+		
+		
+		
+		
 		this.rightTable.add(this.registrationButton);
 		this.rightTable.row();
 		this.rightTable.add(this.showHouseInteractionButton);
@@ -79,10 +201,13 @@ public class AdministrationScreen extends AbstractScreen{
 		this.rightTable.add(this.backButton);
 		this.rightTable.row();
 
+		
+		
 		mainTable.setFillParent(true);
 		// Aggiungo table allo stage
 		stage.addActor(mainTable);
 		stage.addActor(this.rightTable);
+		stage.addActor(centerTable);
 	}
 
 	@Override
@@ -97,14 +222,14 @@ public class AdministrationScreen extends AbstractScreen{
 		this.stage.act();
 		this.stage.draw();
 
-		if( this.registration ) {
+		if (this.registration) {
 			this.registration = false;
 //			Utils.resp = Utils.ADMIN_REGISTERS_A_NEW_USER;
 			Utils.saveOnLog(Utils.ADMIN_REGISTERS_A_NEW_USER);
 			ScreenManager.getInstance().showScreen(new RegistrationCredentialsScreenCreator());
 		}
 
-		if( this.showHouseInteraction ) {
+		if (this.showHouseInteraction) {
 			this.showHouseInteraction = false;
 			/*
 			 * TODO SCREEN PER IL LOG
@@ -112,7 +237,7 @@ public class AdministrationScreen extends AbstractScreen{
 			ScreenManager.getInstance().showScreen(new LogScreenCreator());
 		}
 
-		if( this.back ) {
+		if (this.back) {
 			this.back = false;
 			Utils.showPopUp(Utils.ADMINISTRATION_SCREEN_BACK_POPUP, skin, stage, Utils.MAIN_SCREEN_POP);
 		}
@@ -122,26 +247,25 @@ public class AdministrationScreen extends AbstractScreen{
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 }
